5bcd001ad383bbfe7f038edbf819d79cbd679cd4
Misha Marinenko <40549120+misha-marinenko-official@users.noreply.github.com>
2018-11-17 12:59:54 +0200

    Update freelancehunt.js

--- freelancehunt.js
+++ freelancehunt.js
@@ -1,1 +1,19 @@
+// ==UserScript==
+// @name         Freelancehuntmod
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Freelancehunt modding...
+// @author       You
+// @match        https://freelancehunt.com/*
+// @grant        none
+// ==/UserScript==
 
+(function() {
+    'use strict';
+
+var link = document.createElement("link");
+link.href = "https://raw.githubusercontent.com/misha-marinenko-official/freelancehunt.js/master/freelancehunt.css";
+link.type = "text/css";
+link.rel = "stylesheet";
+document.getElementsByTagName("head")[0].appendChild(link);
+})();
